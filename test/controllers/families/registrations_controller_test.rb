15e4133ec11f7d679c80a1cac9ac6a1d5a47f4aa
oyamairishitomi <h.tezuka0707@gmail.com>
2026-09-08 15:27:05 +0900

    見逃していたテスト用ドメイン(a@a.com)をexample.comへ修正(3-2)

--- test/controllers/families/registrations_controller_test.rb
+++ test/controllers/families/registrations_controller_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class Families::RegistrationsControllerTest < ActionDispatch::IntegrationTest
   test "新規登録するとFamilyが作成される" do
     assert_difference "Family.count", 1 do
-      post families_registrations_path, params: { family: { email: "a@a.com", aikotoba: "aaa", password: "password123", privacy_agreement: "1", terms_agreement: "1" } }
+      post families_registrations_path, params: { family: { email: "a@example.com", aikotoba: "aaa", password: "password123", privacy_agreement: "1", terms_agreement: "1" } }
     end
   end
 end
