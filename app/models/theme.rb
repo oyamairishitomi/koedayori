4d2902930c496245c08408249b796c38696564ef
oyamairishitomi <h.tezuka0707@gmail.com>
2026-09-08 15:42:34 +0900

    Theme.choose でテーマが0件のときのZeroDivisionErrorを防ぐ

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -2,6 +2,8 @@ class Theme < ApplicationRecord
   has_many :posts
 
   def self.choose
+    return if count.zero?
+
     theme_index = Date.current.yday % count
     order(:id).offset(theme_index).first
   end
